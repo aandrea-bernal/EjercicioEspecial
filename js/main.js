ae98a9770a414ff00c9ba8f1e42057c3998f25da
aandrea-bernal <aandreagb27.12@gmail.com>
2024-11-25 10:54:54 -0600

    commit no.2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function()){
+    const loadButton = document.getElementById("load-products-button");
+
+    if (loadButton){
+        loadButton.addEventListener("click", async function () {
+            
+            
+        })
+    }
+}
